5f312f535c12180284e8713df39c47862781a715
“ryuichi24” <ryuichi.nishi24@gmail.com>
2022-02-24 09:11:31 +0200

    add MysqlRepository

---
+++ packages/iremono-backend-core/src/infra/data-access/mysql/repositories/mysql-repository.ts
@@ -0,0 +1,35 @@
+import { Logger, LoggerFactory } from '@iremono/util';
+import { MysqlDatabase } from '..';
+
+export abstract class MysqlRepository<TEntity extends { id: string }> {
+  protected readonly _logger: Logger;
+
+  constructor(loggerFactory: LoggerFactory) {
+    this._logger = loggerFactory.createLogger(this.constructor.name);
+  }
+
+  public async save(entity: TEntity): Promise<TEntity> {
+    const entityExists = !!(await this.findOneById(entity.id));
+
+    if (!entityExists) await this._insert(entity);
+    if (entityExists) await this._update(entity);
+
+    return entity;
+  }
+
+  protected async _query(query: string, values: any[]) {
+    const result = await MysqlDatabase.getConnectionPool().query(query, values);
+    this._logger.debug(
+      `[query="${query}", \nvalues="[${values}]", \nresult="${JSON.stringify(result[0], null, '\t')}"]`,
+    );
+    return result;
+  }
+
+  public abstract findOneById(id: string): Promise<TEntity | null>;
+
+  protected abstract _insert(entity: TEntity): Promise<TEntity | null>;
+
+  protected abstract _update(entity: TEntity): Promise<TEntity | null>;
+
+  protected abstract _toEntity(raw: any): TEntity;
+}
--- packages/iremono-backend-core/src/infra/data-access/mysql/repositories/mysql-user-repository.ts
+++ packages/iremono-backend-core/src/infra/data-access/mysql/repositories/mysql-user-repository.ts
@@ -1,74 +1,60 @@
-import { Logger, LoggerFactory } from '@iremono/util';
-import { MysqlDatabase } from '..';
+import { LoggerFactory } from '@iremono/util';
 import { User } from '../../../../entities';
 import { UserRepository } from '../../../../repositories';
+import { MysqlRepository } from './mysql-repository';
 
-export class MysqlUserRepository implements UserRepository {
-  private readonly _logger: Logger;
-
+export class MysqlUserRepository extends MysqlRepository<User> implements UserRepository {
   constructor(loggerFactory: LoggerFactory) {
-    this._logger = loggerFactory.createLogger(this.constructor.name);
-  }
-
-  public async save(entity: User): Promise<User> {
-    const entityExists = !!(await this.findOneById(entity.id));
-
-    if (!entityExists) await this._insert(entity);
-    if (entityExists) await this._update(entity);
-
-    return entity;
+    super(loggerFactory);
   }
 
   public async findOneByEmail(email: string): Promise<User | null> {
-    const result = await MysqlDatabase.getConnectionPool().query('SELECT * FROM users WHERE email = ?;', [email]);
+    const query = 'SELECT * FROM users WHERE email = ?;';
+    const values = [email];
+    const result = await this._query(query, values);
     const user = (result[0] as any)[0];
     if (!user) return null;
 
-    return new User(
-      {
-        email: user.email,
-        password: user.password,
-        encryptionKeyInitializationVector: user.encryption_key_initialization_vector,
-        createdAt: user.created_at,
-        updatedAt: user.updated_at,
-      },
-      user.id,
-    );
+    return this._toEntity(user);
   }
 
   public async findOneById(id: string): Promise<User | null> {
-    const result = await MysqlDatabase.getConnectionPool().query('SELECT * FROM users WHERE id = ?;', [id]);
+    const query = 'SELECT * FROM users WHERE id = ?;';
+    const values = [id];
+    const result = await this._query(query, values);
+
     const user = (result[0] as any)[0];
     if (!user) return null;
 
+    return this._toEntity(user);
+  }
+
+  protected async _insert(entity: User) {
+    const query = 'INSERT INTO users (id, email, password) VALUES (?, ?, ?);';
+    const values = [entity.id, entity.email, entity.hashedPassword];
+    await this._query(query, values);
+
+    return entity;
+  }
+
+  protected async _update(entity: User) {
+    const query = 'UPDATE users SET email = ?, password = ?, updated_at = ? WHERE id = ?;';
+    const values = [entity.email, entity.hashedPassword, entity.updatedAt, entity.id];
+    await this._query(query, values);
+
+    return entity;
+  }
+
+  protected _toEntity(userRaw: any) {
     return new User(
       {
-        email: user.email,
-        password: user.password,
-        encryptionKeyInitializationVector: user.encryption_key_initialization_vector,
-        createdAt: user.created_at,
-        updatedAt: user.updated_at,
+        email: userRaw.email,
+        password: userRaw.password,
+        encryptionKeyInitializationVector: userRaw.encryption_key_initialization_vector,
+        createdAt: userRaw.created_at,
+        updatedAt: userRaw.updated_at,
       },
-      user.id,
+      userRaw.id,
     );
-  }
-
-  private async _insert(entity: User) {
-    await MysqlDatabase.getConnectionPool().query('INSERT INTO users (id, email, password) VALUES (?, ?, ?);', [
-      entity.id,
-      entity.email,
-      entity.hashedPassword,
-    ]);
-
-    return entity;
-  }
-
-  private async _update(entity: User) {
-    await MysqlDatabase.getConnectionPool().query(
-      'UPDATE users SET email = ?, password = ?, updated_at = ? WHERE id = ?;',
-      [entity.email, entity.hashedPassword, entity.updatedAt, entity.id],
-    );
-
-    return entity;
   }
 }
