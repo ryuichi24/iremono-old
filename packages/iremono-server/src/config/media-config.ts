af4199b088381467cab495cd780078e659cb6fa4
“ryuichi24” <ryuichi.nishi24@gmail.com>
2022-02-27 21:50:16 +0200

    add media dir name option

--- packages/iremono-server/src/config/media-config.ts
+++ packages/iremono-server/src/config/media-config.ts
@@ -1,5 +1,7 @@
 export const mediaConfig = Object.freeze({
   PATH_TO_MEDIA_DIR: process.env.PATH_TO_MEDIA_DIR || './',
+  MEDIA_DIR_NAME: process.env.MEDIA_DIR_NAME || 'iremono_media',
   ENCRYPTION_KEY: process.env.ENCRYPTION_KEY || 'b4c107b3dd6abb389fadc4933a818d45',
-  ENCRYPTION_KEY_FOR_CLIENT_ENCRYPTION_KEY: process.env.ENCRYPTION_KEY_FOR_CLIENT_ENCRYPTION_KEY || 'ecsd7bewswbb389fadc4kkpl933a818d45',
+  ENCRYPTION_KEY_FOR_CLIENT_ENCRYPTION_KEY:
+    process.env.ENCRYPTION_KEY_FOR_CLIENT_ENCRYPTION_KEY || 'ecsd7bewswbb389fadc4kkpl933a818d45',
 });
