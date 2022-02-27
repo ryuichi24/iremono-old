4e23c88acd7b917da2ddbabb98ca6a481cd99217
“ryuichi24” <ryuichi.nishi24@gmail.com>
2022-02-27 23:35:24 +0200

    fix demoModeHandler

--- packages/iremono-server/src/shared/express-lib/middlewares/demo-mode-handler.ts
+++ packages/iremono-server/src/shared/express-lib/middlewares/demo-mode-handler.ts
@@ -3,7 +3,7 @@ import express from 'express';
 export const demoModeHandler = () => (req: express.Request, res: express.Response, next: express.NextFunction) => {
   if (process.env.IS_DEMO_MODE !== 'true') return next();
 
-  if (['POST', 'PUT', 'PATCH', 'DELETE'].includes(req.method))
+  if (['POST', 'PUT', 'PATCH', 'DELETE'].includes(req.method) && req.path !== '/api/auth/signin')
     return res.status(405).json({
       error: {
         message: 'Method not allowed.',
