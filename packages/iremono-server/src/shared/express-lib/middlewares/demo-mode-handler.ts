50600388bd970c10c7b97d3f96d5e47206ec7497
“ryuichi24” <ryuichi.nishi24@gmail.com>
2022-03-03 23:58:23 +0200

    update demoModeHandler

--- packages/iremono-server/src/shared/express-lib/middlewares/demo-mode-handler.ts
+++ packages/iremono-server/src/shared/express-lib/middlewares/demo-mode-handler.ts
@@ -7,14 +7,16 @@ export const demoModeHandler = () => (req: express.Request, res: express.Respons
     ['POST', 'PUT', 'PATCH', 'DELETE'].includes(req.method) &&
     req.path !== '/api/auth/signin' &&
     req.path !== '/api/auth/signout' &&
+    !(/\/api\/files\/.+\/token/.test(req.path)) &&
     req.path !== '/api/auth/refresh-token'
-  )
+  ) {
     return res.status(405).json({
       error: {
         message: 'Method not allowed.',
         timestamp: new Date().toISOString(),
       },
     });
+  }
 
   return next();
 };
