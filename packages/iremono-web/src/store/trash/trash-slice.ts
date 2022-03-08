910990d6bc39e569d58390cc941b0dc1f75d49e1
“ryuichi24” <ryuichi.nishi24@gmail.com>
2022-03-08 21:58:02 +0200

    add trash slice actions and selectors for crypto folder

--- packages/iremono-web/src/store/trash/trash-slice.ts
+++ packages/iremono-web/src/store/trash/trash-slice.ts
@@ -4,11 +4,15 @@ import { RootState } from '..';
 interface TrashState {
   folderTrashItemList: any[];
   fileTrashItemList: any[];
+  cryptoFolderTrashItemList: any[];
+  cryptoFileTrashItemList: any[];
 }
 
 const initialState: TrashState = {
   folderTrashItemList: [],
   fileTrashItemList: [],
+  cryptoFolderTrashItemList: [],
+  cryptoFileTrashItemList: [],
 };
 
 export const trashSlice = createSlice({
@@ -32,6 +36,23 @@ export const trashSlice = createSlice({
         state.fileTrashItemList.splice(indexOfTrashItem, 1);
       }
     },
+    setCryptoTrashItems: (state, { payload }: PayloadAction<{ trashItems: any[] }>) => {
+      const folderTrashItems = payload.trashItems.filter((item) => item.isFolder);
+      const fileTrashItems = payload.trashItems.filter((item) => !item.isFolder);
+      state.cryptoFolderTrashItemList = folderTrashItems;
+      state.cryptoFileTrashItemList = fileTrashItems;
+    },
+    removeCryptoTrashItem: (state, { payload }: PayloadAction<{ trashItem: any }>) => {
+      if (payload.trashItem.isFolder) {
+        const indexOfTrashItem = state.cryptoFolderTrashItemList.findIndex((item) => item.id === payload.trashItem.id);
+        state.cryptoFolderTrashItemList.splice(indexOfTrashItem, 1);
+      }
+
+      if (!payload.trashItem.isFolder) {
+        const indexOfTrashItem = state.cryptoFileTrashItemList.findIndex((item) => item.id === payload.trashItem.id);
+        state.cryptoFileTrashItemList.splice(indexOfTrashItem, 1);
+      }
+    },
   },
 });
 
@@ -44,6 +65,16 @@ export const folderTrashItemListSelector = createSelector(
 export const fileTrashItemListSelector = createSelector(
   (state: RootState) => state.trashState,
   (trashState) => trashState.fileTrashItemList,
+);
+
+export const cryptoFolderTrashItemListSelector = createSelector(
+  (state: RootState) => state.trashState,
+  (trashState) => trashState.cryptoFolderTrashItemList,
+);
+
+export const cryptoFileTrashItemListSelector = createSelector(
+  (state: RootState) => state.trashState,
+  (trashState) => trashState.cryptoFileTrashItemList,
 );
 
 export const trashActions = trashSlice.actions;
--- packages/iremono-web/src/store/trash/use-trash-actions.ts
+++ packages/iremono-web/src/store/trash/use-trash-actions.ts
@@ -6,6 +6,8 @@ import { trashActions } from './trash-slice';
 interface TrashActions {
   setTrashItems: (arg: { trashItems: any[] }) => void;
   removeTrashItem: (arg: { trashItem: any }) => void;
+  setCryptoTrashItems: (arg: { trashItems: any[] }) => void;
+  removeCryptoTrashItem: (arg: { trashItem: any }) => void;
 }
 
 export const useTrashActions = (): TrashActions => {
@@ -25,8 +27,24 @@ export const useTrashActions = (): TrashActions => {
     [dispatch],
   );
 
+  const setCryptoTrashItems = useCallback(
+    (args: { trashItems: any[] }) => {
+      dispatch(trashActions.setCryptoTrashItems(args));
+    },
+    [dispatch],
+  );
+
+  const removeCryptoTrashItem = useCallback(
+    (args: { trashItem: any }) => {
+      dispatch(trashActions.removeCryptoTrashItem(args));
+    },
+    [dispatch],
+  );
+
   return {
     setTrashItems,
     removeTrashItem,
+    setCryptoTrashItems,
+    removeCryptoTrashItem,
   } as const;
 };
