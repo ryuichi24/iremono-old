c1a5043f82297a40d65d081581501c27e399bc09
“ryuichi24” <ryuichi.nishi24@gmail.com>
2022-04-09 23:59:20 +0300

    add test for GetFileTokenUseCase

---
+++ packages/iremono-backend-core/src/use-cases/files/get-file-token/get-file-token-use-case.test.ts
@@ -0,0 +1,130 @@
+import { jest } from '@jest/globals';
+import { StorageItem } from '../../../entities';
+import { constructMockStorageItemRepository } from '../../../infra/data-access/mock/repositories/mock-storage-item-repository';
+import {
+  FileTokenOutput,
+  IDownloadFileTokenService,
+  IStreamFileTokenService,
+  TokenOutput,
+} from '../../../services/token-service';
+import { GetFileTokenRequestDTO } from './contracts';
+import { GetFileTokenUseCase } from './get-file-token-use-case';
+
+const TEST_USER_1_ID = '953a7c7d-efbf-467a-a57c-6be18b2456d7';
+const TEST_ROOT_FOLDER_1_ID = 'f6bbf56a-c4d1-404c-bf5d-66f54be46cca';
+const TEST_FOLDER_1_ID = '1a1cec9e-9bf0-4b93-bcc1-0f2032d73826';
+const TEST_FILE_1_ID = '44bc7bb7-5e8d-4382-bd26-41e8eb87a29e';
+
+const mockItems = [
+  new StorageItem(
+    { name: 'test root folder', isFolder: true, ownerId: TEST_USER_1_ID, parentId: null },
+    TEST_ROOT_FOLDER_1_ID,
+  ),
+  new StorageItem(
+    {
+      name: 'test folder 1',
+      isFolder: true,
+      ownerId: TEST_USER_1_ID,
+      parentId: TEST_ROOT_FOLDER_1_ID,
+    },
+    TEST_FOLDER_1_ID,
+  ),
+  new StorageItem(
+    { name: 'test file 1', isFolder: false, ownerId: TEST_USER_1_ID, parentId: TEST_FOLDER_1_ID },
+    TEST_FILE_1_ID,
+  ),
+];
+
+const mockStorageItemRepository = constructMockStorageItemRepository();
+
+beforeEach(async () => {
+  for (const item of mockItems) {
+    await mockStorageItemRepository.save(item);
+  }
+});
+
+afterEach(async () => {
+  for (const item of mockItems) {
+    await mockStorageItemRepository.remove(item);
+  }
+});
+
+describe('test GetFileTokenUseCase handle method', () => {
+  const DOWNLOAD_FILE_TOKEN = 'c74ac767e09ef9a198441bb363b73f9d';
+  const STREAM_FILE_TOKEN = '6b19ced6e9fba47f13aff4085b1cc661';
+
+  const mockDownloadFileTokenService: IDownloadFileTokenService = {
+    generate: jest.fn((payload: FileTokenOutput): TokenOutput => {
+      return {
+        expiresIn: '999999',
+        value: DOWNLOAD_FILE_TOKEN,
+      };
+    }),
+    verify: jest.fn((token: string): FileTokenOutput => {
+      return {
+        fileId: TEST_FILE_1_ID,
+        clientEncryptionKey: '',
+      };
+    }),
+    revoke: jest.fn((token: string): void => {}),
+  };
+
+  const mockStreamFileTokenService: IStreamFileTokenService = {
+    generate: jest.fn((payload: FileTokenOutput): TokenOutput => {
+      return {
+        expiresIn: '999999',
+        value: STREAM_FILE_TOKEN,
+      };
+    }),
+    verify: jest.fn((token: string): FileTokenOutput => {
+      return {
+        fileId: TEST_FILE_1_ID,
+        clientEncryptionKey: '',
+      };
+    }),
+  };
+
+  it('should return a download file token', async () => {
+    const TOKEN_TYPE = 'download';
+
+    const getFileTokenUseCase = new GetFileTokenUseCase(
+      mockStorageItemRepository,
+      mockDownloadFileTokenService,
+      mockStreamFileTokenService,
+    );
+
+    const mockGetFileTokenUseCaseDTO: GetFileTokenRequestDTO = {
+      id: TEST_FILE_1_ID,
+      ownerId: TEST_USER_1_ID,
+      tokenType: TOKEN_TYPE,
+      clientEncryptionKey: '',
+    };
+
+    const responseDTO = await getFileTokenUseCase.handle(mockGetFileTokenUseCaseDTO);
+
+    expect(responseDTO.tokenType).toBe(TOKEN_TYPE);
+    expect(responseDTO.fileToken?.value).toBe(DOWNLOAD_FILE_TOKEN);
+  });
+
+  it('should return a stream file token', async () => {
+    const TOKEN_TYPE = 'stream';
+
+    const getFileTokenUseCase = new GetFileTokenUseCase(
+      mockStorageItemRepository,
+      mockDownloadFileTokenService,
+      mockStreamFileTokenService,
+    );
+
+    const mockGetFileTokenUseCaseDTO: GetFileTokenRequestDTO = {
+      id: TEST_FILE_1_ID,
+      ownerId: TEST_USER_1_ID,
+      tokenType: TOKEN_TYPE,
+      clientEncryptionKey: '',
+    };
+
+    const responseDTO = await getFileTokenUseCase.handle(mockGetFileTokenUseCaseDTO);
+
+    expect(responseDTO.tokenType).toBe(TOKEN_TYPE);
+    expect(responseDTO.fileToken?.value).toBe(STREAM_FILE_TOKEN);
+  });
+});
